8209da4e9a3d72577b20aa8681828bbba6d9d6c8
zisam shaikh <zisamshaikh7@gmail.com>
2023-09-04 04:08:05 +0600

    Commit

--- src/components/services/ClearAligners/ClearAligners.jsx
+++ src/components/services/ClearAligners/ClearAligners.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ClearAlignersBaner from "./ClearAlignersBaner";
 import ClrarContent from "./ClrarContent"
+import Faq from "./Faq"
 const ClearAligners = () => {
   return (
     <>
     <ClearAlignersBaner />
     <ClrarContent />
+    <Faq/>
     </>
   );
 };
