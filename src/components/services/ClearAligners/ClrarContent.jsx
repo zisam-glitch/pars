74874c7f7c1735927f31668f729561ab9567fdc8
zisam shaikh <zisamshaikh7@gmail.com>
2023-09-04 03:47:12 +0600

    fff

--- src/components/services/ClearAligners/ClrarContent.jsx
+++ src/components/services/ClearAligners/ClrarContent.jsx
@@ -15,34 +15,35 @@ const ClearAlignersBaner = () => {
       </div>
       <div className="tab1 py-10">
         <h1 className="text-3xl	py-4">Benefits of Clear Aligners</h1>
-        <h2 className="text-2xl	pt-2 pb-2">Discreet Appearance</h2>
+        <h2 className="text-2xl	pt-2.5 ">Discreet Appearance</h2>
 
         <p>
           Unlike metal braces, clear aligners are virtually invisible. This
           makes them an excellent option for adults and teens who want to
           improve their smile without drawing attention to their treatment.
         </p> 
-        <h2 className="text-2xl	pt-2 pb-2">Comfort</h2>
+        <h2 className="text-2xl	pt-2.5 ">Comfort</h2>
 
         <p>
           Made of smooth plastic, these aligners are more comfortable than
           traditional braces, which may have sharp wires or brackets.
         </p>
-        <h2 className="text-2xl	pt-2 pb-2">Convenience</h2>
+        <h2 className="text-2xl	pt-2.5 ">Convenience</h2>
 
         <p>
           Clear aligners can be easily removed, making it much easier to eat,
           drink, and maintain good oral hygiene.
         </p>
-        <h2 className="text-2xl	pt-2 pb-2">Customization</h2>
+        <h2 className="text-2xl	pt-2.5 ">Customization</h2>
 
         <p>
           Each set of aligners is custom-made using 3D imaging technology to fit
           snugly over your teeth and guide them to their ideal position.
         </p>
       </div>
+
       <div className="tab1 py-16">
-        <h1 className="text-3xl	py-4">What are Clear Aligners?</h1>
+        <h1 className="text-3xl	py-4">Types of Clear Aligners</h1>
         <p>
           Clear Aligners are transparent plastic trays designed to gradually
           shift your teeth into their ideal positions. Unlike metal braces,
