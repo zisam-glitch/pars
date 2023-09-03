6a7f6e288ed7e289ac252056e8a2d3e36e2aab98
zisam shaikh <zisamshaikh7@gmail.com>
2023-09-04 02:59:11 +0600

    clear all

--- src/components/services/ClearAligners/ClearAligners.jsx
+++ src/components/services/ClearAligners/ClearAligners.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ClearAlignersBaner from "./ClearAlignersBaner";
+import ClrarContent from "./ClrarContent"
 const ClearAligners = () => {
   return (
     <>
     <ClearAlignersBaner />
+    <ClrarContent />
     </>
   );
 };
