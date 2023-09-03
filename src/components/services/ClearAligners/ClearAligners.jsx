006d3a2b710b71a1cc37337eb00898c63dcf6229
zisam shaikh <zisamshaikh7@gmail.com>
2023-09-04 04:45:25 +0600

    commit

--- src/components/services/ClearAligners/ClearAligners.jsx
+++ src/components/services/ClearAligners/ClearAligners.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ClearAlignersBaner from "./ClearAlignersBaner";
 import ClrarContent from "./ClrarContent";
 import Faq from "./Faq";
-import ContactInfo from "./ContactInfo"
+import ContactInfo from "./ContactInfo";
 
 const ClearAligners = () => {
   return (
@@ -10,6 +10,14 @@ const ClearAligners = () => {
       <ClearAlignersBaner />
       <ClrarContent />
       <Faq />
+      <div className="tab1 py-6">
+        <h1 className="text-3xl	py-4">Conclusion</h1>
+        <p>
+          General Dentistry is an integral part of maintaining your overall
+          health. With a focus on preventative care, diagnosis, and treatment,
+          it serves as your first line of defense against oral health issues.
+        </p>
+      </div>
       <ContactInfo />
     </>
   );
