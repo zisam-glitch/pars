8968b0f8cd867c20e682b12c852c4b0d21edf273
zisam shaikh <zisamshaikh7@gmail.com>
2023-09-04 03:20:13 +0600

    commut

--- src/components/services/ClearAligners/ClrarContent.jsx
+++ src/components/services/ClearAligners/ClrarContent.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const ClearAlignersBaner = () => {
   return (
-    <section className="headclear max-w-screen-lg m-auto py-5">
-      <div className="tab1 py-1">
+    <section className="headclear max-w-screen-lg m-auto py-8">
+      <div className="tab1 py-8">
         <h1>What are Clear Aligners?</h1>
         <p>
           Clear Aligners are transparent plastic trays designed to gradually
