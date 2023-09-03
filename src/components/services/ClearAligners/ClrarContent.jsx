c3ad9888a4379e246b2abe59b272fb790051060e
zisam shaikh <zisamshaikh7@gmail.com>
2023-09-04 02:56:30 +0600

    dg

---
+++ src/components/services/ClearAligners/ClrarContent.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+const ClearAlignersBaner = () => {
+  return (
+    <section>
+      <div className="tab1">
+        <h1>What are Clear Aligners?</h1>
+        <p>
+          Clear Aligners are transparent plastic trays designed to gradually
+          shift your teeth into their ideal positions. Unlike metal braces,
+          these aligners are virtually invisible and can be easily removed for
+          eating and cleaning. They have become increasingly popular due to
+          their convenience, aesthetics, and effectiveness.
+        </p>
+      </div>
+      <div className="tab1">
+        <h1>What are Clear Aligners?</h1>
+        <p>
+          Clear Aligners are transparent plastic trays designed to gradually
+          shift your teeth into their ideal positions. Unlike metal braces,
+          these aligners are virtually invisible and can be easily removed for
+          eating and cleaning. They have become increasingly popular due to
+          their convenience, aesthetics, and effectiveness.
+        </p>
+      </div>
+      <div className="tab1">
+        <h1>What are Clear Aligners?</h1>
+        <p>
+          Clear Aligners are transparent plastic trays designed to gradually
+          shift your teeth into their ideal positions. Unlike metal braces,
+          these aligners are virtually invisible and can be easily removed for
+          eating and cleaning. They have become increasingly popular due to
+          their convenience, aesthetics, and effectiveness.
+        </p>
+      </div>
+      <div className="tab1">
+        <h1>What are Clear Aligners?</h1>
+        <p>
+          Clear Aligners are transparent plastic trays designed to gradually
+          shift your teeth into their ideal positions. Unlike metal braces,
+          these aligners are virtually invisible and can be easily removed for
+          eating and cleaning. They have become increasingly popular due to
+          their convenience, aesthetics, and effectiveness.
+        </p>
+      </div>
+      <div className="tab1">
+        <h1>What are Clear Aligners?</h1>
+        <p>
+          Clear Aligners are transparent plastic trays designed to gradually
+          shift your teeth into their ideal positions. Unlike metal braces,
+          these aligners are virtually invisible and can be easily removed for
+          eating and cleaning. They have become increasingly popular due to
+          their convenience, aesthetics, and effectiveness.
+        </p>
+      </div>
+      <div className="tab1">
+        <h1>What are Clear Aligners?</h1>
+        <p>
+          Clear Aligners are transparent plastic trays designed to gradually
+          shift your teeth into their ideal positions. Unlike metal braces,
+          these aligners are virtually invisible and can be easily removed for
+          eating and cleaning. They have become increasingly popular due to
+          their convenience, aesthetics, and effectiveness.
+        </p>
+      </div>
+      
+    </section>
+  );
+};
+
+export default ClearAlignersBaner;
