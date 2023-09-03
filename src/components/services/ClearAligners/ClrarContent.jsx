8f63e3f00f5200a7a71aa6e473e49314231b5b57
zisam shaikh <zisamshaikh7@gmail.com>
2023-09-04 03:39:25 +0600

    commit

--- src/components/services/ClearAligners/ClrarContent.jsx
+++ src/components/services/ClearAligners/ClrarContent.jsx
@@ -13,8 +13,43 @@ const ClearAlignersBaner = () => {
           their convenience, aesthetics, and effectiveness.
         </p>
       </div>
-      <div className="tab1">
-        <h1>What are Clear Aligners?</h1>
+      <div className="tab1 py-16">
+        <h1 className="text-3xl	py-4">Benefits of Clear Aligners</h1>
+        <h2 className="text-2xl	py-2">Discreet Appearance</h2>
+
+        <p>
+          Unlike metal braces, clear aligners are virtually invisible. This
+          makes them an excellent option for adults and teens who want to
+          improve their smile without drawing attention to their treatment.
+        </p>
+        <h2 className="text-2xl	py-2">Discreet Appearance</h2>
+
+        <p>
+          Unlike metal braces, clear aligners are virtually invisible. This
+          makes them an excellent option for adults and teens who want to
+          improve their smile without drawing attention to their treatment.
+        </p>
+        <h2 className="text-2xl	py-2">Comfort</h2>
+
+        <p>
+          Made of smooth plastic, these aligners are more comfortable than
+          traditional braces, which may have sharp wires or brackets.
+        </p>
+        <h2 className="text-2xl	py-2">Convenience</h2>
+
+        <p>
+          Clear aligners can be easily removed, making it much easier to eat,
+          drink, and maintain good oral hygiene.
+        </p>
+        <h2 className="text-2xl	py-2">Customization</h2>
+
+        <p>
+          Each set of aligners is custom-made using 3D imaging technology to fit
+          snugly over your teeth and guide them to their ideal position.
+        </p>
+      </div>
+      <div className="tab1 py-16">
+        <h1 className="text-3xl	py-4">What are Clear Aligners?</h1>
         <p>
           Clear Aligners are transparent plastic trays designed to gradually
           shift your teeth into their ideal positions. Unlike metal braces,
@@ -23,8 +58,8 @@ const ClearAlignersBaner = () => {
           their convenience, aesthetics, and effectiveness.
         </p>
       </div>
-      <div className="tab1">
-        <h1>What are Clear Aligners?</h1>
+      <div className="tab1 py-16">
+        <h1 className="text-3xl	py-4">What are Clear Aligners?</h1>
         <p>
           Clear Aligners are transparent plastic trays designed to gradually
           shift your teeth into their ideal positions. Unlike metal braces,
@@ -33,8 +68,8 @@ const ClearAlignersBaner = () => {
           their convenience, aesthetics, and effectiveness.
         </p>
       </div>
-      <div className="tab1">
-        <h1>What are Clear Aligners?</h1>
+      <div className="tab1 py-16">
+        <h1 className="text-3xl	py-4">What are Clear Aligners?</h1>
         <p>
           Clear Aligners are transparent plastic trays designed to gradually
           shift your teeth into their ideal positions. Unlike metal braces,
@@ -43,27 +78,6 @@ const ClearAlignersBaner = () => {
           their convenience, aesthetics, and effectiveness.
         </p>
       </div>
-      <div className="tab1">
-        <h1>What are Clear Aligners?</h1>
-        <p>
-          Clear Aligners are transparent plastic trays designed to gradually
-          shift your teeth into their ideal positions. Unlike metal braces,
-          these aligners are virtually invisible and can be easily removed for
-          eating and cleaning. They have become increasingly popular due to
-          their convenience, aesthetics, and effectiveness.
-        </p>
-      </div>
-      <div className="tab1">
-        <h1>What are Clear Aligners?</h1>
-        <p>
-          Clear Aligners are transparent plastic trays designed to gradually
-          shift your teeth into their ideal positions. Unlike metal braces,
-          these aligners are virtually invisible and can be easily removed for
-          eating and cleaning. They have become increasingly popular due to
-          their convenience, aesthetics, and effectiveness.
-        </p>
-      </div>
-      
     </section>
   );
 };
