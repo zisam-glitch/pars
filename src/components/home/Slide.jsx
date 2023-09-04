8157e2adeac9fe25ff6298e2ffd979748d75f6f2
zisam shaikh <zisamshaikh7@gmail.com>
2023-09-04 09:10:23 +0600

    commit

--- src/components/home/Slide.jsx
+++ src/components/home/Slide.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
+import Typewriter from "typewriter-effect";
 
 function HeroSlide() {
   return (
     <>
-     <p className="fff">fff</p>
-    </>
+    <Swiper
+      slidesPerView={1}
+      spaceBetween={0}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="mySwiper"
+    >
+      <SwiperSlide>
+        <div className="bg-gray-500">
+            <p>ff</p>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+      <div className="bg-gray-900">
+            <p>ff</p>
+        </div>
+      </SwiperSlide>
+    </Swiper>
+  </>
   );
 }
 export default HeroSlide;
