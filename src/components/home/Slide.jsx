72bbc45f3cb5ed5f76935558d24c6602f3075513
zisam shaikh <zisamshaikh7@gmail.com>
2023-09-04 09:03:33 +0600

    commit

--- src/components/home/Slide.jsx
+++ src/components/home/Slide.jsx
@@ -1,15 +1,9 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import { Pagination } from "swiper";
-const Bnraa = () => {
-  return (
-     <div className="moe">
-        <p className="ff">
-            ff
-        </p>
-     </div>
-  );
-};
 
-export default Bnraa;
+function HeroSlide() {
+  return (
+    <>
+     <p className="fff">fff</p>
+    </>
+  );
+}
+export default HeroSlide;
