56accf06a463d5e212f28620eef479999817fb9a
zisam shaikh <zisamshaikh7@gmail.com>
2023-09-04 08:57:46 +0600

    commit

--- src/components/home/Slide.jsx
+++ src/components/home/Slide.jsx
@@ -2,9 +2,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination } from "swiper";
-const Bnr = () => {
+const Bnraa = () => {
   return (
-    <>
       <Swiper
         slidesPerView={1}
         spaceBetween={0}
@@ -16,17 +15,16 @@ const Bnr = () => {
       >
         <SwiperSlide>
           <div className="bg-gray-500 p-10">
-
+            <p>aa</p>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="bg-gray-900 p-10" >
-
+          <div className="bg-gray-900 p-10">
+            <p>vv</p>
           </div>
         </SwiperSlide>
       </Swiper>
-    </>
   );
 };
 
-export default Bnr;
+export default Bnraa;
